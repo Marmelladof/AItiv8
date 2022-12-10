64c5adf4605ca245c222e133416c640d47536dbf
Pxdr0-A <pedromfa98@gmail.com>
2022-12-10 00:57:01 +0000

    polished datatypes and tested fit method

--- ml_section/model_training/__main__.py
+++ ml_section/model_training/__main__.py
@@ -10,6 +10,8 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
 import pre_processing as pp
+import utils
+import elipsoide_model as epm
 
 def main():
     # read dataset
@@ -17,22 +19,31 @@ def main():
     df = pd.read_csv("./resources/Crop_recommendation.csv")
 
     status = pp.check_dataset(df)
-    print(status)
 
     outliers_dict = pp.outlier_detection(df)
-    print(outliers_dict)
     
     # separation of independent and dependent variable and label encoding
     X = df.drop("label", axis=1)
     y = df["label"]
     le = LabelEncoder()
-    y = le.fit_transform(y)
+    y_encoded = le.fit_transform(y)
+    labels_dict = utils.dictify_labels(le.inverse_transform(y_encoded), y_encoded)
+    # converting back into a dataframe
+    y_encoded = pd.DataFrame(y_encoded, columns=["label"])
 
     # adding interactions and selecting most important features
     X_inter = pp.add_interactions(X)
     selector = SelectKBest(chi2, k=len(df.columns.values)-1)
     X_inter_reduced = selector.fit_transform(X_inter,y)
-    print(f"Selected Features: {X_inter.columns.values[selector.get_support()]}")
+    selected_features = X_inter.columns.values[selector.get_support()]
+    # converting back into a dataframe
+    X_inter_reduced = pd.DataFrame(X_inter_reduced, columns=selected_features)
+
+    model1 = epm.ElipsoideModel()
+    model1.fit(X,y_encoded)
+
+    #ponto_aleatorio_arroz = [85, 58, 41, 21.770462, 80.319644, 7.038096, 226.655537]
+    #ponto_aleatorio_milho = [107, 34, 32, 26.774637, 66.413269, 6.780064, 177.774507]
 
 if __name__ == "__main__":
     main()
