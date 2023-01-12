04dfb8e9b0eb832032fd2d97bb055caec6379d73
Francisco Ferreira <franciscoferreira7@pm.me>
2023-01-12 22:17:44 +0000

    Added Redux. Changed the logic for adding and sending forms.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import store from "./Store.jsx";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
