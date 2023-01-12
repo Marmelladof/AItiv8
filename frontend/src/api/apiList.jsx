36ebef327c541b42752744443d541ffb79d74ebd
Francisco Ferreira <franciscoferreira7@pm.me>
2023-01-12 22:30:47 +0000

    Changed to http

--- frontend/src/api/apiList.jsx
+++ frontend/src/api/apiList.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const baseUrl = "https://localhost:8000/";
+const baseUrl = "http://localhost:8000/";
 
 export const postCrop = async (payload) => {
-  // let url = baseUrl + "crop_type";
-  let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io"
+   let url = baseUrl + "crop_type";
+  // let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io"
   return await axios.post(url, payload);
 };
 
