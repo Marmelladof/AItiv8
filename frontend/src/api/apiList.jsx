41519bceb3c5eabe9cb1ca303df97a05ee1a0ab9
Marmelladof <120035479+Marmelladof@users.noreply.github.com>
2023-01-15 16:49:52 +0000

    url fix

--- frontend/src/api/apiList.jsx
+++ frontend/src/api/apiList.jsx
@@ -3,14 +3,14 @@ import axios from "axios";
 const baseUrl = "http://localhost:8000/";
 
 export const postCrop = async (payload) => {
-  //  let url = baseUrl + "crop_type";
-  let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io";
+   let url = baseUrl + "crop_type";
+  // let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io";
   return await axios.post(url, payload);
 };
 
 export const postPlanning = async (payload) => {
-  // let url = baseUrl + "optimized_planning";
-  let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io";
+  let url = baseUrl + "optimized_planning";
+  // let url = "https://85febffd-a07e-4718-8df8-0164313ee004.mock.pstmn.io";
   return await axios.post(url, payload);
 };
 
